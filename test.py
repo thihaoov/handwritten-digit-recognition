3be1a5e739fafa92ec91f10dc5e4aca08e7922b4
Arees12 <thihaoo1251998@gmail.com>
2022-01-21 21:00:09 +0630

    First commit

---
+++ test.py
@@ -0,0 +1,19 @@
+# import os
+from typing import final
+# import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+
+mnist = tf.keras.datasets.mnist
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+x_train = tf.keras.utils.normalize(x_train, axis=1)
+x_test = tf.keras.utils.normalize(x_test, axis=1)
+
+model = tf.keras.models.load_model('handwritten.model')
+
+loss, accuracy = model.evaluate(x_test, y_test)
+
+print(loss)
+print(accuracy)
